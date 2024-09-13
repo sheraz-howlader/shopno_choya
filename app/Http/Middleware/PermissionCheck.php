2d56462abd28fd84d600a8bb2a91f226ab6f806d
Sheraz Howlader <mdsherazhowlader@gmail.com>
2024-09-13 15:13:58 +0600

    make permission cache

--- app/Http/Middleware/PermissionCheck.php
+++ app/Http/Middleware/PermissionCheck.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Models\Permission;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Gate;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -19,7 +20,11 @@ class PermissionCheck
             }
         });
 
-        $permissions = Permission::all();
+        //$permissions = Permission::all();
+        $permissions = Cache::remember('permissions', 60, function () {
+            return Permission::get();
+        });
+
         foreach ($permissions as $permission) {
             Gate::define($permission->action, function ($user) use ($permission) {
                 return $user->role->permissions()->pluck('action')->contains($permission->action);
